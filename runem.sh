7f80eaa3643f475012b948f5e41d23f857eee6cc
Peeter Joot <peeterjoot@pm.me>
2021-01-31 10:05:58 -0500

    switch runem to use binary output

--- runem.sh
+++ runem.sh
@@ -1,2 +1,2 @@
-time ./mpauli --xmin -2.0 --xmax 0.6 --ymin -1.2 --ymax 1.2 --maxiter 400 > ~/Downloads/mp.txt 
-time ./mcomplex --xmin -2.0 --xmax 0.6 --ymin -1.2 --ymax 1.2 --maxiter 400 > ~/Downloads/mc.txt 
+time ./mpauli --xmin -2.0 --xmax 0.6 --ymin -1.2 --ymax 1.2 --maxiter 400 --output ~/Downloads/mp.txt
+time ./mcomplex --xmin -2.0 --xmax 0.6 --ymin -1.2 --ymax 1.2 --maxiter 400 --output ~/Downloads/mc.txt
